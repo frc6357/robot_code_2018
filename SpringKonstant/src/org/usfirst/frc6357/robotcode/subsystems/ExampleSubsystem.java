8fa4d6b754f9154d18202b347e9b15c2e2a93a73
hoo761 <magnahoo761@gmail.com>
2018-01-24 18:56:12 -0600

    Formatted code, removed RobotMap

--- SpringKonstant/src/org/usfirst/frc6357/robotcode/subsystems/ExampleSubsystem.java
+++ SpringKonstant/src/org/usfirst/frc6357/robotcode/subsystems/ExampleSubsystem.java
@@ -5,12 +5,14 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  *
  */
-public class ExampleSubsystem extends Subsystem {
+public class ExampleSubsystem extends Subsystem 
+{
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-    public void initDefaultCommand() {
+    public void initDefaultCommand() 
+    {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
