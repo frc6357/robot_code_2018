72f147178dd5764a50f644363725ccd3616fc077
RG8452 <ryang8452@yahoo.com>
2018-02-10 13:19:58 -0600

    Finished deadband filter

--- SpringKonstant/src/org/usfirst/frc6357/robotcode/tools/filters/FilterDeadband.java
+++ SpringKonstant/src/org/usfirst/frc6357/robotcode/tools/filters/FilterDeadband.java
@@ -7,21 +7,43 @@ package org.usfirst.frc6357.robotcode.tools.filters;
 public class FilterDeadband extends Filter
 {
     private double deadband;    //The deadbanding for the input, equal to distance from zero
+    private double slope;       //The slope m of the y=mx line to get careful control
     
     /**
      * The consructor for a filter with a deadband
-     * @param deadband 
+     * @param deadband the deadbanding zone, equivalent to the distance from zero
      */
     public FilterDeadband(double deadband)
     {
-        this.deadband = deadband;
+        setDeadband(deadband);
     }
 
+    /**
+     * Filters the raw input for a deadbanding zone
+     * If the given value is inside the deadband, zero is returned
+     * Otherwise, a scale is implemented and slope is readjusted for careful control
+     */
     @Override
     public double filter(double rawAxis)
     {
-        // TODO Auto-generated method stub
-        return 0;
+        if(!(Math.abs(rawAxis) < deadband))
+        {
+            return (slope * (rawAxis - deadband));
+        }
+        else
+        {
+            return 0;
+        }
     }
 
+    /**
+     * Sets the deadband of the filter to a new deadband, with length equal to distance to zero
+     * Also adjust the slope of the y=mx line for better functionality
+     * @param deadband
+     */
+    public void setDeadband(double deadband)
+    {
+        this.deadband = deadband;
+        slope = 1 / (1 - deadband);
+    }
 }
