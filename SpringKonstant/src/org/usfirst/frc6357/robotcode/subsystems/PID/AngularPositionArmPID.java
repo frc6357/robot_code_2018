1bd6686852d5e43850d91f6ab6eb8b002f3b55ed
Dave Wilson <dave_wilson@apple.com>
2018-02-20 20:08:23 -0600

    More TODO comments in the ARM angle controller.

--- SpringKonstant/src/org/usfirst/frc6357/robotcode/subsystems/PID/AngularPositionArmPID.java
+++ SpringKonstant/src/org/usfirst/frc6357/robotcode/subsystems/PID/AngularPositionArmPID.java
@@ -20,6 +20,12 @@ public class AngularPositionArmPID implements PIDOutput
         mySpeedController = inSpeedController;
         myDistanceMeasurment = inDistanceMeasurment;
 
+        // TODO: The pidOutput parameter (mySpeedController here) must be "this" because you
+        // want the controller to call the local pidWrite function and not set the motor speed
+        // directly. Setting the motors speed directly would bypass the limit switch test and
+        // that wouldn't be a great idea. I suggest you pass in the ARM subsystem object and
+        // use that in pidWrite(), calling the ARMSubsystem.periodic() function and passing
+        // the speed parameter to pidWrite() to it.
         myPidController = new PIDController(Kp, Ki, Kd, myDistanceMeasurment, mySpeedController);
         myPidController.setOutputRange(-1, 1);
     }
