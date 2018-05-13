93827d24a7edf02d681b55746c25c58446e1802c
Dave Wilson <dave_wilson@apple.com>
2018-05-13 16:46:49 -0500

    Tidied up TFMini code and move to MXP serial port
    Removed redundant code.
    Added commenting in the state machine.
    Removed tabs from source.

--- SpringKonstant/src/org/usfirst/frc6357/robotcode/subsystems/TFMini.java
+++ SpringKonstant/src/org/usfirst/frc6357/robotcode/subsystems/TFMini.java
@@ -1,14 +1,12 @@
 package org.usfirst.frc6357.robotcode.subsystems;
 
-import org.usfirst.frc6357.robotcode.Robot;
-
 import edu.wpi.first.wpilibj.SerialPort;
 
 /**
- * This is a handler class for the TFMini LiDAR sensor for the FRC WPIlib 
+ * This is a handler class for the TFMini LiDAR sensor for the FRC WPIlib
  * SerialPort class. The sensor returns packets of 9 bytes. Each byte has a
  * use.
- * 
+ *
  * SENSOR INFORMATION
  *  ╔════════════╦═════════╗
  *  ║ Protocol   ║ UART    ║
@@ -21,7 +19,7 @@ import edu.wpi.first.wpilibj.SerialPort;
  *  ╠════════════╬═════════╣
  *  ║ Parity bit ║ 0       ║
  *  ╚════════════╩═════════╝
- *  
+ *
  *  DATA FORMAT of SERIAL PORT
  *  ╔════════╦═════════════╗
  *  ║ byte 1 ║ 0x59        ║
@@ -32,7 +30,7 @@ import edu.wpi.first.wpilibj.SerialPort;
  *  ╠════════╬═════════════╣
  *  ║ byte 4 ║ Dist_H      ║
  *  ╠════════╬═════════════╣
- *  ║ byte 5 ║ Strength_L  ║ 
+ *  ║ byte 5 ║ Strength_L  ║
  *  ╠════════╬═════════════╣
  *  ║ byte 6 ║ Strength_H  ║
  *  ╠════════╬═════════════╣
@@ -42,72 +40,69 @@ import edu.wpi.first.wpilibj.SerialPort;
  *  ╠════════╬═════════════╣
  *  ║ byte 9 ║ Check Sum   ║
  *  ╚════════╩═════════════╝
- * 
+ *
  * @author hoo761
  */
 
 public class TFMini
 {
-    private final SerialPort sensor;		// WPI serial port class
-    
+    private final SerialPort sensor;        // WPI serial port class
+
     // Serial port information
-    private final int PACKET_LENGTH = 9;	// Length of the Packets/Data that the sensor sends back
-    private final byte startByte = 0x59;	// This byte is the start of a UART message, also == to the double 89 (byte 1, and 2)
-    private final int baudRate = 115200;	// The baud rate to configure the serial port
-    private final int dataBits = 8;			// The number of data bits per transfer
-    private final SerialPort.StopBits stopBits = SerialPort.StopBits.kOne;
-    private final SerialPort.Parity parity = SerialPort.Parity.kNone;
-    private final SerialPort.Port port = SerialPort.Port.kOnboard; //Port used to plug in TFMini
-    
+    private final int PACKET_LENGTH = 9;    // Length of the Packets/Data that the sensor sends back
+    private final byte startByte = 0x59;    // This byte is the start of a UART message, also == to the double 89 (byte 1, and 2)
+    private final int baudRate = 115200;    // The baud rate to configure the serial port
+    private final int dataBits = 8;         // The number of data bits per transfer
+
     // This is a list of bytes for the initialization of the LiDAR
     private final byte[] config = {0x42, 0x57, 0x02, 0x00, 0x00, 0x00, 0x01, 0x06};
-    
+
     // Data bytes
     private int distance;    //Distance being read
     private int strength;    //Strength of signal
     private int quality;     //Quality of signal
     private int checksum;    // byte 9: checksum
-    
-    private boolean startRead;		// Boolean to start or stop reading values
-    private ReceivingState state;   // State of the packet receive
-    private byte[] packet = new byte[9];    // Buffer for one packet
+
+    private boolean startRead;              // Boolean to start or stop reading values
+    public ReceivingState state;            // State of the packet receive
+    public byte[] packet = new byte[9];     // Buffer for one packet
     private int count = 0;
-    
+
     /**
      * Constructor, creates an instance of Serial Port class.
      * @param - SerialPort.Port enum, pick where it is plugged into
      */
     public TFMini()
     {
-        sensor = new SerialPort(baudRate, port, dataBits, parity, stopBits);
+        sensor = new SerialPort(baudRate, SerialPort.Port.kMXP, dataBits, SerialPort.Parity.kNone, SerialPort.StopBits.kOne);
         state = ReceivingState.WAIT_START1;
-        initilize();
+        initialize();
     }
-    
+
     private enum ReceivingState
     {
         WAIT_START1,
         WAIT_START2,
         RECEIVING,
     }
-    
+
     /**
      * Starts the reading of numbers from the LiDAR
      */
     public void start()
     {
-    	startRead = true;
-    	runLiDAR();
+        startRead = true;
+        runLiDAR();
     }
-    
+
     /**
      * Stops the reading of the LiDAR
      */
     public void stop()
     {
-    	startRead = false;
+        startRead = false;
     }
-    
+
     /**
      * Returns the distance. By By multiplying distH by 256, the binary data is
      * shifted by 8 to the left. Now the lower 8-bit distance data, distL is added
@@ -116,9 +111,9 @@ public class TFMini
      */
     public int getDistance()
     {
-    	return distance;
+        return distance;
     }
-    
+
     /**
      * Returns the strength of the signal
      * @return strength
@@ -127,44 +122,35 @@ public class TFMini
     {
         return strength;
     }
-        
+
     /**
      * Gets the signal quality degree byte as a double
      * @return - signal quality degree byte
      */
     public int getSignalQualityDegree()
     {
-    	return quality;
+        return quality;
     }
-    
+
     /**
-     * This forwards the byte list of a configuration command that is 
+     * This forwards the byte list of a configuration command that is
      * required to receive data from it (part of the initialization process).
-     * Also sets the timeout.
+     * Also sets the timeout to be 4 packet times.
      */
-    private void initilize()
+    private void initialize()
     {
-    	sensor.write(config, config.length);
-    	sensor.setReadBufferSize(PACKET_LENGTH);
-    	sensor.setTimeout(4/100);
+        sensor.write(config, config.length);
+        sensor.setReadBufferSize(PACKET_LENGTH);
+        sensor.setTimeout(4/100);
     }
-    
-    /**
-     * Gets the number of bytes currently available to read from the serial port
-     * @return - The number of bytes available to read
-     */
-    private int amountOfBytes()
-    {
-        return sensor.getBytesReceived();
-    }
-    
+
     /**
      * Runs a loop in a Thread that will read the 9 byte packets and set each
      * byte to the correct piece of data for use if the data is valid.
      */
     private void runLiDAR()
     {
-    	new Thread(() ->
+        new Thread(() ->
         {
             try
             {
@@ -176,41 +162,62 @@ public class TFMini
                        switch(state)
                        {
                            case WAIT_START1:
-                               if(bytes[0] == 0x59)
+                               if(bytes[0] == startByte)
                                {
+                                   // We've read our first 0x59 (packet start marker) so set things up just in case
+                                   // this really is the start of a packet. Clear our byte counter, initialize the
+                                   // checksum and store the first byte.
                                    count = 0;
                                    checksum = bytes[0];
-                                   packet[0] = bytes[0];
+                                   packet[count] = bytes[0];
+
+                                   // Start looking for the next character which we hope is another 0x59.
                                    state = ReceivingState.WAIT_START2;
                                }
                                break;
                            case WAIT_START2:
-                               if(bytes[0] == 0x59)
+                               if(bytes[0] == startByte)
                                {
+                                   // We received a second 0x59 so this really looks like a new packet.
+                                   // Store the byte, update the checksum and byte counter then start reading
+                                   // bytes until we reach the end of the packet.
                                    count++;
                                    checksum += bytes[0];
-                                   packet[1] = bytes[0];
+                                   packet[count] = bytes[0];
                                    state = ReceivingState.RECEIVING;
                                }
                                else
                                {
+                                   // This wasn't an 0x59 so we are not at the start of a packet. Go back and
+                                   // start looking for a new packet header.
                                    state = ReceivingState.WAIT_START1;
                                }
                                break;
                            case RECEIVING:
+                               // Save this byte in our packet buffer.
                                count++;
                                packet[count] = bytes[0];
+
+                               // Have we read a whole packet of data?
                                if(count == PACKET_LENGTH - 1)
                                {
-                                   // Reached the end of the packet, check checksum
+                                   // Reached the end of the packet, so the byte we just received is the
+                                   // checksum. Make sure it agrees with the checksum we've been accumulating
+                                   // as we read packet bytes.
                                    if(checksum == bytes[0])
                                    {
+                                       // Checksum is good so extract the distance, strength and quality info from
+                                       // the received packet.
                                        parsePacket();
                                    }
+
+                                   // Start looking for the start of the next packet.
                                    state = ReceivingState.WAIT_START1;
                                }
                                else
                                {
+                                   // We're not at the end of the packet so just update our checksum
+                                   // accumulator and keep receiving.
                                    checksum += bytes[0];
                                }
                                break;
@@ -221,43 +228,11 @@ public class TFMini
             catch(Exception e) {}
         }).start();
     }
-    
+
     public void parsePacket()
     {
         distance = (((int) packet[3] * 256) + ((int) packet[2]));
         strength = (((int) packet[5] * 256) + ((int) packet[4]));
         quality = (int)packet[7];
-    }
-    
-    /**
-     * Checks if the packet of 9 bytes is a data packet and if so, has to pass
-     * the checksum.
-     * @param bytes - array of bytes
-     * @return - true if starting two bytes are equal to 0x59 (the starting bytes)
-     * and passes the checksum.
-     */
-    private boolean isValid(byte[] bytes)
-    {
-    	return bytes[0] == startByte 
-    			&& bytes[1] == startByte 
-    			&& checkChecksum(bytes, bytes[9]);
-    }
-    
-    /**
-     * The checksum is the sum of byte1 - byte8. This checks if the checksum
-     * is equal to the sum of the bytes.
-     * @param bytes - array of bytes
-     * @param checkSum - the checksum byte
-     * @return true if checksum is equal to the sum of previous bytes
-     */
-    private boolean checkChecksum(byte[] bytes, byte checkSum)
-    {
-    	double total = 0;
-    	
-    	for(int i = 0; i < dataBits; i++)
-    	{
-    		total += (double) bytes[i];
-    	}
-    	return total == (double) checkSum;
     }
 }
